29c7f8f60a90a02289602a9f743f164b7589aa58
Jake Flynn <jake@myflynns.com>
2018-11-09 08:27:27 -0800

    adding files

---
+++ client/src/NotFound.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const NotFound = () => (
+	
+  <div className="main-content not-found">
+    <div className="flex">
+    	<div className="flex-item">	
+    		<h2>Page Not Found... but good try</h2>
+    	</div>
+    </div>
+  </div>
+);
+
+export default NotFound;
